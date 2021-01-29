c4f7a52855794af2fc1f6793ad46bb9f531da292
Musatics Gilbert <musaticsgilbert@gmail.com>
2021-01-29 18:51:21 +0100

    change to forked repo to forward events to custom tags

--- src/ImageBook/ImageBook.jsx
+++ src/ImageBook/ImageBook.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import HTMLFlipBook from "react-pageflip";
+import HTMLFlipBook from "m-react-pageflip";
 import "./ImageBook.scss";
 
 const PageCover = React.forwardRef((props, ref) => {
@@ -48,8 +48,10 @@ const Page = React.forwardRef((props, ref) => {
     console.log(e);
   }
 
-  function showFullImage() {
-    console.log('click');
+  function showFullImage(e) {
+    console.log('props', props);
+    console.log('ref', ref);
+    props.onImageClick(e);
   }
 });
 
@@ -69,7 +71,7 @@ class ImageBook extends React.Component {
         </ol>
       </Page>,
       <Page key={2} number={1} imageCaption={"Önéletrajz"}>
-        <article style={{ padding: '2%' }}>
+        {/* <article style={{ padding: '2%' }}>
           Alim Adilov is a painter, was born in 1963, Taskent, Uzbekistan.
           During his elementary studies he was enrolled to the best drawing course in the city.<br />
           In 1979 he started the secondary art school, where strict but great knowledged teachers taught him, e.g. the Korean Emil-Ki- Gajt, who did several successful book illustrations. In his 5th year, he learned on advanced level, he grew up as an artist who is ready to work full responsibility.<br />
@@ -81,12 +83,22 @@ class ImageBook extends React.Component {
           He is the member of the Hungarian Art Association.<br />
           His paintings are often bought because this way the buyers can take home a slice from the East, from their holiday. The vivid colors of the paintings are reflectiong the beach, the market, so the owners can feel these moments in their weekdays.<br />
           Favourably, he works on those topics he experienced on his journey. Cities, landscapes, houses but also portraits, still lifes.All over the world he had solo and organised exhibitions, also he has a permanent exhibition on Madeira
+        </article> */}
+        <article style={{ padding: '2%' }}>
+          Alim Adilov Üzbegisztán fõvárosában, Taskentben született 1963-ban és gyerekkorában elkezdett járni mûvészeti szakkörre, ahol Zója Grigorevna kiváló festõmûvésznõ tanította. 8 éves iskolai végzettség után Taskentben 4 éves Mûvészeti technikumot végzett.
+          Katonai szolgálat után felvételizett az európai hirû szentpétervári "Repin" Képzõmûvészeti Akadémiara és ott 6 évig tanult, közben részt vett csoportos kiállításokban Szovjetunióban és számos külföldi országban. Akadémia elvégzése után elutazott Londonba, ahol részt vett a "Canvas" mûvészeti tanfolyamban.
+          <br />1993 óta hazankban él és alkot. 2004-tõl a Magyar Alkotomûvészek Országos Egyesületének tagja. Többnyire vegyes technikával, vegyes témákban dolgozik. Mûvésztelepeknek rendszeres tagja.
+          <br />"Alim Adilov festõmûvész rohanó életünk elé olyan festményeket állít, melyekbõl sugárzik a természet varázsa és a Teremtõ jósága. Képei, mint bástyák dacolnak a háborgó világ forgatagával, idõk és korok múlásával."
+          <br />
+          Csoportos kiállitások: Szentpétervár, Moszkva, Rosztov, Taskent, Tokyo, Kuala Lumpur, Budapest, Debrecen, Békéscsaba
+          Önálló kiállítások: London, Békéscsaba, Miskolc, Debrecen, Gödöllõ, Kecskemét, Budapest, Malaysia (Puchong), Kuala Lumpur
+          Állandó kiállítás: Madeira
         </article>
       </Page>,
-      <Page key={3} number={2} image={process.env.PUBLIC_URL + "/h608.jpg"} imageCaption={"Tengerparton"}></Page>,
+      <Page key={3} number={2} image={process.env.PUBLIC_URL + "/h608.jpg"} imageCaption={"Tengerparton"} onImageClick={this.renderFullScreen}></Page>,
       <Page key={4} number={3} image={process.env.PUBLIC_URL + "/h503.jpg"} imageCaption={"Cserépedények"}></Page>,
       <Page key={5} number={4} image={process.env.PUBLIC_URL + "/h607.jpg"} imageCaption={"Virágok az ablakban"}></Page>,
-      <Page key={6} number={5} image={process.env.PUBLIC_URL + "/f1014.jpg"} imageCaption={"Délben"}></Page>,
+      <Page key={6} number={5} image={process.env.PUBLIC_URL + "/f1014.jpg"} imageCaption={"Átjáró"}></Page>,
       <PageCover key={7} pos="bottom">  </PageCover>
     ]
 
@@ -107,8 +119,14 @@ class ImageBook extends React.Component {
     this.flipBook.getPageFlip().flipPrev();
   };
 
-  renderFullScreen = () => {
-    document.getElementsByTagName('HTMLFlipBook')[0].requestFullscreen();
+  renderFullScreen = (e) => {
+    // console.log(e.target);
+    console.log(this.flipBook);
+    this.flipBook.pageFlip.setting.useMouseEvents = false;
+    e.target.requestFullscreen().then(() => {
+
+      console.log(document.fullscreenElement);
+    });
   }
 
   onPage = (e) => {
@@ -124,6 +142,7 @@ class ImageBook extends React.Component {
   }
 
   onChangeState = (e) => {
+    console.log(e);
     this.setState({
       state: e.data,
     });
@@ -139,7 +158,9 @@ class ImageBook extends React.Component {
 
   render() {
     return (
-      <div className="container-md no-select book-container" style={{ maxHeight: '100vh', maxWidth: '100vw', display: 'flex', flexDirection: 'column', alignItems: 'center', position: 'relative' }}>
+      <div className="container-md no-select book-container"
+      // style={{ maxHeight: '100vh', maxWidth: '100vw', display: 'flex', flexDirection: 'column', alignItems: 'center', position: 'relative' }}
+      >
         <HTMLFlipBook
           width={700}
           height={900}
@@ -151,12 +172,15 @@ class ImageBook extends React.Component {
           maxShadowOpacity={0.5}
           showCover={true}
           mobileScrollSupport={true}
+          flippingTime={700}
+          clickEventForward={['img', 'button', 'a']}
           drawShadow={true}
           onFlip={this.onPage}
           onChangeOrientation={this.onChangeOrientation}
           onChangeState={this.onChangeState}
+          swipeDistance={20}
           className="image-book flip-book html-book"
-          // style={{ backgroundImage: "url(https://cssauthor.com/wp-content/uploads/2015/10/Old-painted-wood.jpg)" }}
+          useMouseEvents={true}
 
           ref={(el) => (this.flipBook = el)}
         >
