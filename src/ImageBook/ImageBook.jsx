bdf9329a616fe92589aa3792b50ce04bf3140d21
Musatics Gilbert <musaticsgilbert@gmail.com>
2021-02-02 18:44:13 +0100

    refactor Page

--- src/ImageBook/ImageBook.jsx
+++ src/ImageBook/ImageBook.jsx
@@ -1,61 +1,7 @@
 import React from 'react'
 import HTMLFlipBook from "m-react-pageflip";
 import "./ImageBook.scss";
-
-const PageCover = React.forwardRef((props, ref) => {
-  return (
-    <div className={"page page-cover page-cover-" + props.pos} ref={ref} data-density="hard" >
-      <div className="page-content" >
-        <h2>{props.children} </h2>
-      </div>
-    </div>
-  );
-});
-
-const Page = React.forwardRef((props, ref) => {
-  return (
-    <div className="page" ref={ref} data-density={props.density | "soft"}>
-      <div className="page-content">
-        <h2 className="page-header">{props.imageCaption}</h2>
-        {/* {(props.image !== undefined) ?
-          <div
-            className="page-image"
-            style={{ backgroundImage: "url(" + props.image + ")" }}
-            onClick={showFullImage}
-          ></div> : null
-        } */}
-        {(props.image !== undefined) ?
-          <div className="page-image">
-            <img
-              alt={props.imageCaption}
-              src={props.image}
-              // style={{ backgroundImage: "url(" + props.image + ")", objectFit: 'contain' }}
-              onClick={showFullImage}
-              onLoad={setDimensions}
-            ></img>
-          </div>
-          : null
-        }
-        {/* <figcaption>{props.imageCaption}</figcaption> */}
-        {(props.children) ? <div className="page-text">{props.children}</div> : null}
-        <div className="page-footer">{isNaN(props.number) ? '' : props.number}</div>
-      </div>
-    </div>
-  );
-
-  function setDimensions(e) {
-    e.target.style = (e.target.width > e.target.height) ? 'width: 100%' : 'height: 100%';
-    console.log(e);
-  }
-
-  function showFullImage(e) {
-    console.log('props', props);
-    console.log('ref', ref);
-    e.target.requestFullscreen().then(() => {
-
-    });
-  }
-});
+import { Page, PageCover } from "./Page/Page";
 
 class ImageBook extends React.Component {
   constructor(props) {
---
+++ src/ImageBook/Page/Page.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+
+const PageCover = React.forwardRef((props, ref) => {
+  return (
+    <div className={"page page-cover page-cover-" + props.pos} ref={ref} data-density="hard" >
+      <div className="page-content" >
+        <h2>{props.children} </h2>
+      </div>
+    </div>
+  );
+});
+
+const Page = React.forwardRef((props, ref) => {
+  return (
+    <div className="page" ref={ref} data-density={props.density | "soft"}>
+      <div className="page-content">
+        <h2 className="page-header">{props.imageCaption}</h2>
+        {/* {(props.image !== undefined) ?
+          <div
+            className="page-image"
+            style={{ backgroundImage: "url(" + props.image + ")" }}
+            onClick={showFullImage}
+          ></div> : null
+        } */}
+        {(props.image !== undefined) ?
+          <div className="page-image">
+            <img
+              alt={props.imageCaption}
+              src={props.image}
+              // style={{ backgroundImage: "url(" + props.image + ")", objectFit: 'contain' }}
+              onClick={showFullImage}
+              onLoad={setDimensions}
+            ></img>
+          </div>
+          : null
+        }
+        {/* <figcaption>{props.imageCaption}</figcaption> */}
+        {(props.children) ? <div className="page-text">{props.children}</div> : null}
+        <div className="page-footer">{isNaN(props.number) ? '' : props.number}</div>
+      </div>
+    </div>
+  );
+
+  function setDimensions(e) {
+    e.target.style = (e.target.width > e.target.height) ? 'width: 100%' : 'height: 100%';
+    console.log(e);
+  }
+
+  function showFullImage(e) {
+    console.log('props', props);
+    console.log('ref', ref);
+    e.target.requestFullscreen().then(() => {
+
+    });
+  }
+});
+
+export { PageCover, Page };
